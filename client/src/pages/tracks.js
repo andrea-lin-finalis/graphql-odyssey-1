c0d5c6b1419b7275ffcb0ae6782735670451676a
Andrea Lin <andrea.lin@finalis.com>
2022-11-23 14:00:57 -0300

    feat(client): query with useQuery

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { gql } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
 
-import { Layout } from '../components';
+import TrackCard from '../containers/track-card';
+import { Layout, QueryResult } from '../components';
 
+// TRACKS gql query to retrieve all tracks
 const TRACKS = gql`
   query GetTracks {
     tracksForHome {
@@ -25,7 +27,17 @@ const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult data={data} error={error} loading={loading}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
