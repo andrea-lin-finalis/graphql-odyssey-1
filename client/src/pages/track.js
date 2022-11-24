7558083874959e1d4ae9abf89cb00ad062c360c2
Andrea Lin <andre93.lin@gmail.com>
2022-11-24 17:08:42 -0300

    feat(client): add useQuery hook with variables

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
+
+import TrackDetail from '../components/track-detail';
 import { Layout, QueryResult } from '../components';
 
+// GET_TRACK gql query to retrieve a specific track by its ID
 export const GET_TRACK = gql`
   query GetTrack($trackId: ID!) {
     track(id: $trackId) {
@@ -26,8 +29,19 @@ export const GET_TRACK = gql`
   }
 `;
 
+// Track page fetches a track's data from the gql query GET_TRACK and provides it to the TrackDetail component to display
 const Track = ({ trackId }) => {
-  return <Layout></Layout>;
+  const { error, loading, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        {/* this is where our componenet displaying the track data will go */}
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Track;
