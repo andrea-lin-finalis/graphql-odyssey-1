99d673c654006d765fcbe19b53d41bc0f754ef46
Andrea Lin <andre93.lin@gmail.com>
2022-11-24 14:31:20 -0300

    feat(server):  add resolver args parameter

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,8 +1,12 @@
 const resolvers = {
   Query: {
-    //returns an array of Tracks that will be used to populate the homepage grid of our web client
+    // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
+    },
+    // get a single track by ID, for the track page
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);
     },
   },
   Track: {
